0c4147bd6d4f4eebe9ea5048c342a0ae9eeb285f
Jacopo Grazzini <gjacopo@gmail.com>
2018-04-06 01:17:37 +0200

    init file created

---
+++ place2nuts/__init__.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+.. __init__.py
+
+Simple API to Eurostat GISCO web-services.
+
+**About**
+
+*credits*:      `gjacopo <jacopo.grazzini@ec.europa.eu>`_ 
+
+*version*:      0.1
+--
+*since*:        Thu Apr  5 16:40:31 2018
+
+
+**Description**
+
+**Usage**
+
+    >>> import place2nuts
+    
+"""
+
+__all__ = ['settings', 'place2nuts']#analysis:ignore
