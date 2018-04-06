dfffca78a9082d2866681e48f1ee96119f337141
Jacopo Grazzini <gjacopo@gmail.com>
2018-04-06 11:31:08 +0200

    updated structure

---
+++ tests/__init__.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+.. _mod_tests__init__
+
+Testing units for API to Eurostat GISCO web-services.
+
+**About**
+
+*credits*:      `gjacopo <jacopo.grazzini@ec.europa.eu>`_ 
+
+*version*:      0.1
+--
+*since*:        Fri Apr  6 11:27:15 2018
+
+
+**Description**
+
+**Usage**
+
+    >>> from place2nuts import tests
+    
+"""
+
+__all__ = ['settings', 'place2nuts']#analysis:ignore
