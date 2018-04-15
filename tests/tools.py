c4655fe93c7a27c13db66d73b58f0498fb24d111
Jacopo Grazzini <gjacopo@gmail.com>
2018-04-16 00:53:52 +0200

    added tests

--- tests/tools.py
+++ tests/tools.py
@@ -55,7 +55,7 @@ except ImportError:
     pass
 
 from happygisco import settings
-from happygisco.tools import _GeoLocation, GeoTool, GDALTool
+from happygisco.tools import _GeoLocation, GeoDistance, GeoAngle, GeoLocation, GDALTool
 
 #==============================================================================
 # TESTING UNITS
@@ -100,9 +100,79 @@ class _GeoLocationTestCase(unittest.TestCase):
 
 
 #/****************************************************************************/
-# GeoToolTestCase
+# GeoDistanceTestCase
 #/****************************************************************************/
-class GeoToolTestCase(unittest.TestCase):
+class GeoDistanceTestCase(unittest.TestCase):
+
+    module = 'tools'
+
+    #/************************************************************************/
+    def setUp(self):
+        self.tol = 0.1 # np.spacing(np.single(1e2)) 
+
+    #/************************************************************************/
+    def test_convert_units(self):
+        """
+        """
+        # test the conversion operators
+        self.assertEqual(GeoDistance.units_to('m', 'km',  dist=1000),            
+                         1)
+        self.assertEqual(GeoDistance.units_to('mi', 'ft',  10.),      
+                         10.*GeoDistance.MI_TO[GeoDistance.FT_DIST_UNIT])
+        self.assertEqual(GeoDistance.units_to('km', 'mi',  100.),     
+                         100.*GeoDistance.KM_TO[GeoDistance.MI_DIST_UNIT])
+        self.assertEqual(GeoDistance.convert_distance_units('m', **{'km':1,  'm':10}),     
+                         1010)
+        dist = GeoDistance.convert_distance_units('m', mi=2,  ft=10, km=0.5)
+        self.assertEqual(2*GeoDistance.MI_TO[GeoDistance.M_DIST_UNIT]           \
+                         + 10.*GeoDistance.FT_TO[GeoDistance.M_DIST_UNIT]       \
+                         + 0.5*GeoDistance.KM_TO[GeoDistance.M_DIST_UNIT],
+                         dist)
+        self.assertEqual(GeoDistance.convert_distance_units('m', mi=2,  **{'ft':10, 'km':0.5}),
+                         dist) 
+
+    #/************************************************************************/
+    def test_distance(self):
+        """
+        """
+        self.assertEqual(GeoDistance.estimate_radius_WGS84(0.),  
+                         GeoDistance.EARTH_RADIUS_EQUATOR)
+        self.assertEqual(GeoDistance.estimate_radius_WGS84(math.pi/2.), 
+                         GeoDistance.EARTH_RADIUS_POLAR)
+        # test some geolocation utilities
+        loc1 = GeoAngle.from_degrees(26.062951, -80.238853)        
+        loc2 = GeoAngle.from_degrees(26.060484,-80.207268)
+        dist_a = GeoDistance.distance_to_from((loc1.deg_lat,loc1.deg_lon), (loc2.deg_lat,loc2.deg_lon), 
+                                            rad=False, unit='km')
+        dist_b = loc1.distance_to(loc2)
+        self.assertLessEqual(np.abs(dist_a - dist_b),
+                             self.tol)                                
+
+#/****************************************************************************/
+# GeoAngleTestCase
+#/****************************************************************************/
+class GeoAngleTestCase(unittest.TestCase):
+
+    module = 'tools'
+
+    #/************************************************************************/
+    def test_convert_units(self):
+        """
+        """
+        # test the conversion operators
+        self.assertLessEqual(np.abs(GeoAngle.ang_units_to('deg', 'rad', ang=180) - 3.14159265359),
+                             self.tol)
+        self.assertLessEqual(np.abs(GeoAngle.ang_units_to('rad', 'deg', ang=3.14159265359) - 180),
+                             self.tol)       
+        self.assertEqual(GeoAngle.deg2rad(90),
+                         math.pi/2.)
+        self.assertEqual(GeoAngle.rad2deg(math.pi), 
+                         180.)
+
+#/****************************************************************************/
+# GeoLocationTestCase
+#/****************************************************************************/
+class GeoLocationTestCase(unittest.TestCase):
 
     module = 'tools'
 
@@ -118,56 +188,10 @@ class GeoToolTestCase(unittest.TestCase):
         self.bounding_box_Ll = [[48.81554, 2.2241], [48.81554, 2.4699], 
                                 [48.90214, 2.4699], [48.90214, 2.2241]]
                             # [lL[::-1] for lL in self.bounding_box] # like the one used in twitter
-
-    #/************************************************************************/
-    def test_convert_units(self):
-        """
-        """
-        # test the conversion operators
-        self.assertEqual(GeoTool.units_to('m', 'km',  dist=1000),            
-                         1)
-        self.assertEqual(GeoTool.units_to('mi', 'ft',  10.),      
-                         10.*GeoTool.MI_TO[GeoTool.FT_DIST_UNIT])
-        self.assertEqual(GeoTool.units_to('km', 'mi',  100.),     
-                         100.*GeoTool.KM_TO[GeoTool.MI_DIST_UNIT])
-        self.assertEqual(GeoTool.convert_distance_units('m', **{'km':1,  'm':10}),     
-                         1010)
-        dist = GeoTool.convert_distance_units('m', mi=2,  ft=10, km=0.5)
-        self.assertEqual(2*GeoTool.MI_TO[GeoTool.M_DIST_UNIT]           \
-                         + 10.*GeoTool.FT_TO[GeoTool.M_DIST_UNIT]       \
-                         + 0.5*GeoTool.KM_TO[GeoTool.M_DIST_UNIT],
-                         dist)
-        self.assertEqual(GeoTool.convert_distance_units('m', mi=2,  **{'ft':10, 'km':0.5}),
-                         dist) 
-        self.assertLessEqual(np.abs(GeoTool.ang_units_to('deg', 'rad', ang=180) - 3.14159265359),
-                             self.tol)
-        self.assertLessEqual(np.abs(GeoTool.ang_units_to('rad', 'deg', ang=3.14159265359) - 180),
-                             self.tol)       
-        self.assertEqual(GeoTool.deg2rad(90),
-                         math.pi/2.)
-        self.assertEqual(GeoTool.rad2deg(math.pi), 
-                         180.)
-
-    #/************************************************************************/
-    def test_distance(self):
-        """
-        """
-        self.assertEqual(GeoTool.estimate_radius_WGS84(0.),  
-                         GeoTool.EARTH_RADIUS_EQUATOR)
-        self.assertEqual(GeoTool.estimate_radius_WGS84(math.pi/2.), 
-                         GeoTool.EARTH_RADIUS_POLAR)
-        # test some geolocation utilities
-        loc1 = GeoTool.from_degrees(26.062951, -80.238853)        
-        loc2 = GeoTool.from_degrees(26.060484,-80.207268)
-        dist_a = GeoTool.distance_to_from((loc1.deg_lat,loc1.deg_lon), (loc2.deg_lat,loc2.deg_lon), 
-                                            rad=False, unit=GeoTool.DEF_DIST_UNIT)
-        dist_b = loc1.distance_to(loc2)
-        self.assertLessEqual(np.abs(dist_a - dist_b),
-                             self.tol)                                
         
     #/************************************************************************/
     def test_bbox(self):
-        lLr = GeoTool.bbox2latlon(self.bbox)
+        lLr = GeoLocation.bbox2latlon(self.bbox)
         self.assertEqual(lLr[:2], 
                          self.lLr[:2])        
         self.assertLessEqual(abs(lLr[2] - self.lLr[2]),
@@ -176,7 +200,7 @@ class GeoToolTestCase(unittest.TestCase):
         # CIRCUMcirle of the bounding box 
         # GeoTool.latlon2bbox returns the bounding box whose INcircle is the
         # circle defined by parameters (lat,Lon,rad)
-        self.assertTrue(GeoTool.bboxwithin(self.bbox, GeoTool.latlon2bbox(*self.lLr)))
-        self.assertEqual(GeoTool.bbox2polygon(self.bbox), self.bounding_box)
-        self.assertEqual(GeoTool.polygon2bbox(self.bounding_box), self.bbox)
-        self.assertEqual(GeoTool.bbox2polygon(self.bbox,order='Ll'), self.bounding_box_Ll)
+        self.assertTrue(GeoLocation.bboxwithin(self.bbox, GeoLocation.latlon2bbox(*self.lLr)))
+        self.assertEqual(GeoLocation.bbox2polygon(self.bbox), self.bounding_box)
+        self.assertEqual(GeoLocation.polygon2bbox(self.bounding_box), self.bbox)
+        self.assertEqual(GeoLocation.bbox2polygon(self.bbox,order='Ll'), self.bounding_box_Ll)
