6a4e1fb91446f012eca0ee6cb8a5d94cca510fdc
gjacopo <gjacopo@gmail.com>
2018-06-27 17:06:53 +0200

    updated doc

--- happygisco/features.py
+++ happygisco/features.py
@@ -732,11 +732,12 @@ class NUTS(_Feature):
     Notes
     -----
     Bulk datasets:
+        
     * bulk download: http://ec.europa.eu/eurostat/cache/GISCO/distribution/v2/nuts/download/
     * list of datasets: http://ec.europa.eu/eurostat/cache/GISCO/distribution/v2/nuts/datasets.json
     NUTS units:
-    * `units 2016 <http://ec.europa.eu/eurostat/cache/GISCO/distribution/v2/nuts/nuts-2016-units.html>`_
-    listed in the `json file <http://ec.europa.eu/eurostat/cache/GISCO/distribution/v2/nuts/nuts-2016-units.json>`
+    * `units 2016 http://ec.europa.eu/eurostat/cache/GISCO/distribution/v2/nuts/nuts-2016-units.html
+    listed in the `json file <http://ec.europa.eu/eurostat/cache/GISCO/distribution/v2/nuts/nuts-2016-units.json>`_
     """
 
     #/************************************************************************/
